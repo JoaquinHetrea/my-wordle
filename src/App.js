3949f88b40de5ee190fccb0d01f32aa3785d14da
JoaquinHetrea <joahetrea@gmail.com>
2022-05-01 23:42:48 -0300

    Added defining solution logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [solution, setSolution] = useState(null)
+
+  //Corre en cada render
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+    .then(res => res.json())
+    .then(json => {
+      const randomSolution = json[Math.floor(Math.random() * json.length)]
+      setSolution(randomSolution.word)
+    })
+  }, /*dependency array*/[setSolution])
+
   return (
     <div className="App">
       <h1>Wordle</h1>
+      {solution && <div>Solution is: { solution }</div>}
     </div>
   );
 }
